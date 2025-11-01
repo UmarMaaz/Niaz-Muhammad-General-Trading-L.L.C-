bceeb1b2e13e3d8ea037cf357af6c3fd587d6b47
Umar Maaz <umarmaaz2637@gmail.com>
2025-11-01 14:01:57 +0500

    fix: make map full width on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -53,10 +53,8 @@ export default function ContactPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-foreground mb-12 text-center">Our Location</h2>
           <div className="w-full h-96 rounded-lg overflow-hidden shadow-lg">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d366.93535476156626!2d71.53398379493842!3d33.967590841136065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sMain%20office%20%20Niaz%20enterprises%20Near%20sarhad%20University%20opposite%20fahad%20CNG%20Peshawar%20ring%20road!5e1!3m2!1sen!2s!4v1761986258741!5m2!1sen!2s" width="800" height="600" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d366.93535476156626!2d71.53398379493842!3d33.967590841136065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sMain%20office%20%20Niaz%20enterprises%20Near%20sarhad%20University%20opposite%20fahad%20CNG%20Peshawar%20ring%20road!5e1!3m2!1sen!2s!4v1761986258741!5m2!1sen!2s" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
-        </div>
-      </section>
 
       <Footer />
     </main>
